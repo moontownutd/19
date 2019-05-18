133e87d53c16c349c2dfa8cb524af25c459eb8d8
kjssong <ssonggyu@gmail.com>
2019-05-18 13:30:20 +0900

    update

--- resource/js/default.js
+++ resource/js/default.js
@@ -6,13 +6,16 @@
 
 
     var layout = {
-        bdy: null,
         btnGnb: null,
 
         init: function() {
             layout.btnGnb = $('.btn__menu');
             layout.loadLayout();
-            layout.bdy.on('click', '.btn__menu', layout.gnbOnOff);
+
+            $('#header').load(function() {
+                layout.btnGnb.on('click', layout.gnbOnOff);
+            });
+
         },
         loadLayout: function() {
             $("#header").load("../layout/header.html");
